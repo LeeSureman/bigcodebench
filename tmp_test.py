e969a906b3f28e8eb78b5416d6e2f2ff08aeb22a
LeeSureman <1349342500@qq.com>
2025-05-09 23:04:14 +0800

    init

--- tmp_test.py
+++ tmp_test.py
@@ -1,22 +1,27 @@
+import argparse
 from gradio_client import Client, handle_file
 
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument('--')
+
+
     gradio_endpoint = "https://bigcode-bigcodebench-evaluator.hf.space/"
 
     results, pass_at_k = client.predict(
-        split=split,
-        subset=subset,
-        samples=handle_file(samples),
-        pass_k=pass_k,
-        parallel=parallel,
-        min_time_limit=min_time_limit,
-        max_as_limit=max_as_limit,
-        max_data_limit=max_data_limit,
-        max_stack_limit=max_stack_limit,
-        calibrated=calibrated,
-        check_gt_only=check_gt_only,
-        no_gt=no_gt,
-        selective_evaluate=selective_evaluate,
+        split='instruct',
+        subset='hard',
+        samples=handle_file('/home/xiaonan/mycode/bigcodebench/tmp_generation.jsonl'),
+        pass_k=1,
+        parallel=-1,
+        min_time_limit=1,
+        max_as_limit=30720,
+        max_data_limit=30720,
+        max_stack_limit=20,
+        calibrated=True,
+        check_gt_only=False,
+        no_gt=False,
+        selective_evaluate=None,
         api_name="/predict"
     )
